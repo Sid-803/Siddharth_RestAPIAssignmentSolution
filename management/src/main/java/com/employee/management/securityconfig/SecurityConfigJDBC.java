8086413e95573aae5fad84c7a61be4a49b85c0ad
Siddhartha <siddharthchauhan888@gmail.com>
2022-07-26 02:13:23 +0530

    invoked csfr

--- management/src/main/java/com/employee/management/securityconfig/SecurityConfigJDBC.java
+++ management/src/main/java/com/employee/management/securityconfig/SecurityConfigJDBC.java
@@ -1,4 +1,13 @@
-//package com.employee.management.securityconfig;
+package com.employee.management.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 //
 //import javax.sql.DataSource;
 //
@@ -13,23 +22,16 @@
 //import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 //
-//@EnableWebSecurity
-//public class SecurityConfigJDBC extends WebSecurityConfigurerAdapter {
-//
-//	@Autowired
-//	DataSource dataSource;
-//
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		// PasswordEncoder encoder =
-//		// PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
-//				.withUser(User.withUsername("Siddharth").password(getPasswordEncoder().encode("Siddharth")).roles("STORE"));
-//
-//	}
-//
-//	@Bean
-//	PasswordEncoder getPasswordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//}
+@EnableWebSecurity
+public class SecurityConfigJDBC extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().formLogin();
+	}
+
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
